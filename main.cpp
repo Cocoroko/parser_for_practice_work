069c3bffbfd686c8476ece3caf158cbfdb0c248a
Esya <56122211+cocoroko@users.noreply.github.com>
2021-01-04 19:03:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,108 +1,110 @@
-#include <vector>
-#include <algorithm>
-#include <cassert>
-#include <string>
-#include <stdexcept>
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <cassert>
+#include <algorithm>
+#include <stdexcept>
+
+using std::vector;
+using std::string;
 
 int getOperand(vector<int> &stack) {
-	if (stack.empty()) {
-		throw std::out_of_range("stack is empty!");
-	}
-	int operand = stack.back();
-	stack.pop_back();
-	return operand;
+  if (stack.empty()) {
+    throw std::out_of_range("stack is empty!");
+  }
+  int operand = stack.back();
+  stack.pop_back();
+  return operand;
 }
 
 
-int getLongestPrefixLength(string rpn, string word) {
-	if (rpn.empty()) {
-		throw logic_error("regular expression is empty!");
-	}
-	rpn.erase(remove_if(rpn.begin(), rpn.end(), ::isspace), rpn.end());
-	int rpn_length = rpn.length();
-	int word_length = word.length();
-	vector<vector<vector<int >>> dp(rpn_length, vector<vector<int >>(word_length + 1, vector<int>(word_length + 1, false)));
+int getLongestPrefixLength(string regular_expression, string word) {
+  if (regular_expression.empty()) {
+    std::cout << "INF" << std::endl;
+    return -1;
+  }
+  regular_expression.erase(remove_if(regular_expression.begin(), regular_expression.end(), ::isspace), regular_expression.end());
+  vector<vector<vector<int >>> dp(regular_expression.length(), vector<vector<int >>(word.length() + 1, vector<int>(word.length() + 1, false)));
 
-	vector<int> stack;
-	try {
-		for (int k = 0; k < rpn_length; ++k) {
-			switch (rpn[k]) {
-				case '+': {
-					int operand2 = getOperand(stack);
-					int operand1 = getOperand(stack);
-					for (int i = 0; i <= word_length; ++i) {
-						for (int j = i; j <= word_length; ++j) {
-							dp[k][i][j] = dp[operand1][i][j] || dp[operand2][i][j];
-						}
-					}
-					break;
-				}
-				case '.': {
-					int operand2 = getOperand(stack);
-					int operand1 = getOperand(stack);
-					for (int i = 0; i <= word_length; ++i) {
-						for (int j = i; j <= word_length; ++j) {
-							for (int delimeter = i; delimeter <= j; ++delimeter) {
-								dp[k][i][j] |= dp[operand1][i][delimeter] && dp[operand2][delimeter][j];
-							}
-						}
-					}
-					break;
-				}
-				case '*': {
-					int operand = getOperand(stack);
-					for (int i = 0; i <= word_length; ++i) {
-						dp[k][i][i] = true;
-					}
-					for (int i = 0; i <= word_length; ++i) {
-						for (int j = i + 1; j <= word_length; ++j) {
-							for (int delimeter = i; delimeter <= j; ++delimeter) {
-								dp[k][i][j] |= dp[k][i][delimeter] && dp[operand][delimeter][j];
-							}
-						}
-					}
-					break;
-				}
-				case '1':
-					for (int i = 0; i <= word_length; ++i) {
-						dp[k][i][i] = true;
-					}
-					break;
-				default: 
-					if (!('a' <= rpn[k] && rpn[k] <= 'c')) {
-						char buffer[rpn_length + 100];
-						int size = snprintf(buffer, sizeof(buffer), "string '%s' contains invalid symbol '%c' (code %d) at position %d", rpn.c_str(), rpn[k], (int) rpn[k], k);
-						assert(size <= sizeof(buffer));
-						throw logic_error(buffer);
-					}
-					for (int i = 0; i < word_length; ++i) {
-						if (word[i] == rpn[k]) {
-							dp[k][i][i + 1] = true;
-						}
-					}
-			}
-			stack.push_back(k);
-		}
-	} catch (out_of_range e) {
-		throw logic_error("string '" + rpn + "' ERROR");
-	}
-	assert(!stack.empty());
-	if (stack.size() > 1) {
-		throw logic_error("string '" + rpn + "' ERROR");
-	}
-	for (int maximumPrefixLength = word_length; maximumPrefixLength >= 0; --maximumPrefixLength) {
-		if (dp[rpn_length - 1][0][maximumPrefixLength]) {
-			return maximumPrefixLength;
-		}
-	}
-	return 0;
+  vector<int> stack;
+  try {
+    for (int k = 0; k < regular_expression.length(); ++k) {
+      switch (regular_expression[k]) {
+        case '1':
+          for (int i = 0; i <= word.length(); ++i) {
+            dp[k][i][i] = true;
+          }
+          break;
+        case '+': {
+          int operand2 = getOperand(stack);
+          int operand1 = getOperand(stack);
+          for (int i = 0; i <= word.length(); ++i) {
+            for (int j = i; j <= word.length(); ++j) {
+              dp[k][i][j] = dp[operand1][i][j] || dp[operand2][i][j];
+            }
+          }
+          break;
+        }
+        case '.': {
+          int operand2 = getOperand(stack);
+          int operand1 = getOperand(stack);
+          for (int i = 0; i <= word.length(); ++i) {
+            for (int j = i; j <= word.length(); ++j) {
+              for (int delimeter = i; delimeter <= j; ++delimeter) {
+                dp[k][i][j] |= dp[operand1][i][delimeter] && dp[operand2][delimeter][j];
+              }
+            }
+          }
+          break;
+        }
+        case '*': {
+          int operand = getOperand(stack);
+          for (int i = 0; i <= word.length(); ++i) {
+            dp[k][i][i] = true;
+          }
+          for (int i = 0; i <= word.length(); ++i) {
+            for (int j = i + 1; j <= word.length(); ++j) {
+              for (int delimeter = i; delimeter <= j; ++delimeter) {
+                dp[k][i][j] |= dp[k][i][delimeter] && dp[operand][delimeter][j];
+              }
+            }
+          }
+          break;
+        }
+        default:
+          if (!('a' <= regular_expression[k] && regular_expression[k] <= 'c')) {
+            std::cout << "INF" << std:: endl;
+            return -1;
+          }
+          for (int i = 0; i < word.length(); ++i) {
+            if (word[i] == regular_expression[k]) {
+              dp[k][i][i + 1] = true;
+            }
+          }
+      }
+      stack.push_back(k);
+    }
+  } catch (std::out_of_range e) {
+    std::cout << "ERROR" << std::endl;
+    return -1;
+  }
+  assert(!stack.empty());
+  if (stack.size() > 1) {
+    std::cout << "ERROR" << std::endl;
+    return -1;
+  }
+  for (int maxPrefLength = word.length(); maxPrefLength >= 0; --maxPrefLength) {
+    if (dp[regular_expression.length() - 1][0][maxPrefLength]) {
+      return maxPrefLength;
+    }
+  }
+  return 0;
 }
 
 int main() {
-	string rpn, word;
-	cin >> rpn >> word;
-	cout << getLongestPrefixLength(rpn, word) << endl;
-	return 0;
+  string regular_expression;
+  std::cin >> regular_expression;
+  string word;
+  std::cin >> word;
+  std::cout << getLongestPrefixLength(regular_expression, word) << std::endl;
+  return 0;
 }
