ca89955d3b49bab37413ff80b9e1066638e9c372
Esya <56122211+cocoroko@users.noreply.github.com>
2021-01-04 18:25:36 +0300

    Update test.h

--- test.h
+++ test.h
@@ -5,23 +5,23 @@ using namespace std;
 int getLongestPrefixLength(string rpn, string word);
 
 void test(string rpn, string word, int answer_right = -1) {
-	int answer = get_longest_prefix_length(rpn, word);
-	if (answer != answer_right) {
-		dbg(rpn, word, answer, answer_right);
-	}
-	assert(answer == answer_right);
+  int answer = getLongestPrefixLength(rpn, word);
+  if (answer != answer_right) {
+    dbg(rpn, word, answer, answer_right);
+  }
+  assert(answer == answer_right);
 }
 
 void test() {
-	test("ab + c.aba. * .bac. + . + *", "abacb", 4);
-	test("acb..bab.c. * .ab.ba. + . + *a.", "cb", 0);
-	test("aca..", "ackba", 0);
-	test("ac1..", "ackba", 2);
-	test("ab1+.c.", "", 0);
-	test("ab1+.c.", "acqwerfsd", 2);
+  test("ab+c.aba.*.bac.+.+*", "abacb", 4);
+  test("acb..bab.c.*.ab.ba.+.+*a.", "cb", 0);
+  test("aca..", "ackba", 0);
+  test("ac1..", "ackba", 2);
+  test("ab1+.c.", "", 0);
+  test("ab1+.c.", "acqwerfsd", 2);
 }
 
 int main() {
-	test();
-	return 0;
+  test();
+  return 0;
 }
